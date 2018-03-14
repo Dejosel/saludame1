f695326c90e4e3e7fc70c8bc5f7fb08a13aab0fc
Dejosel <nubelibreweb@gmail.com>
2018-03-14 10:08:32 -0500

    Update solution.rb

--- solution.rb
+++ solution.rb
@@ -4,9 +4,9 @@ require 'sinatra/base'
 class MyApp < Sinatra::Base
   get "/" do
   	unless params[:name]
-  		"<H1>Hola desconocido...</H1>"
+  		"<H1>Hola desconocido!</H1>"
   	else 
-  		"<H1>Hola #{params[:name]}</H1>"
+  		"<H1>Hola #{params[:name]}!</H1>"
   	end
   end
 
